6b565e6482f87dbc25ea0b572a79505419121bbe
Phuoc <phuocpeter19@gmail.com>
2016-06-30 15:18:33 -0400

    Make GraphicsHierarchy works

--- Assignment2/GraphicsHierarchy.java
+++ Assignment2/GraphicsHierarchy.java
@@ -10,11 +10,6 @@ import acm.graphics.*;
 
 public class GraphicsHierarchy extends GraphicsProgram {
 	
-	final int BOX_WIDTH = 150;
-	final int BOX_HEIGHT = 50;
-	
-	double lineXi, lineYi;
-	
 	public void run() {
 		drawGObject();
 		drawGLabel();
@@ -23,43 +18,87 @@ public class GraphicsHierarchy extends GraphicsProgram {
 		drawGRect();
 	}
 	
-	private void drawLineTo() {
-		
+	/* Method: drawLineTo() */
+	/**
+	 * Draw a line between GRects.
+	 * @param Xf the X destination
+	 * @param Yf the Y destination
+	 */
+	private void drawLineTo(double Xf, double Yf) {
+		GLine line = new GLine(Xi, Yi, Xf, Yf);
+		add(line);
 	}
 	
+	/* Method: drawGRect() */
+	/**
+	 * Draws GRect and labels it.
+	 */
 	private void drawGRect() {
 		GRect box = drawBox();
 		box.move(this.getWidth() - BOX_WIDTH - BOX_WIDTH / 3, this.getHeight() * 3 / 4);
 		drawLabel("GLabel", box);
-		drawLineTo();
+		double Xf = box.getX() + box.getWidth() / 2;
+		double Yf = box.getY();
+		drawLineTo(Xf, Yf);
 	}
 
+	/* Method: drawGOval() */
+	/**
+	 * Draws GOval and labels it.
+	 */
 	private void drawGOval() {
 		GRect box = drawBox();
 		box.move(this.getWidth() / 2 + 9, this.getHeight() * 3 / 4);
 		drawLabel("GOval", box);
+		double Xf = box.getX() + box.getWidth() / 2;
+		double Yf = box.getY();
+		drawLineTo(Xf, Yf);
 	}
 
+	/* Method: drawGLine() */
+	/**
+	 * Draws GLine and labels it.
+	 */
 	private void drawGLine() {
 		GRect box = drawBox();
 		box.move(this.getWidth() / 2 - BOX_WIDTH - 9, this.getHeight() * 3 / 4);
 		drawLabel("GLine", box);
+		double Xf = box.getX() + box.getWidth() / 2;
+		double Yf = box.getY();
+		drawLineTo(Xf, Yf);
 	}
 
+	/* Method: drawGLabel() */
+	/**
+	 * Draws GLabel and labels it.
+	 */
 	private void drawGLabel() {
 		GRect box = drawBox();
 		box.move(BOX_WIDTH / 3, this.getHeight() * 3 / 4);
 		drawLabel("GLabel", box);
+		double Xf = box.getX() + box.getWidth() / 2;
+		double Yf = box.getY();
+		drawLineTo(Xf, Yf);
 	}
 
+	/* Method: drawGObject() */
+	/**
+	 * Draws GObject and labels it.
+	 */
 	private void drawGObject() {
 		GRect box = drawBox();
-		box.move(this.getWidth() / 2 - BOX_WIDTH / 2, this.getHeight() / 4);
+		box.move(this.getWidth() / 2 - BOX_WIDTH / 2, this.getHeight() / 4 - BOX_HEIGHT);
 		drawLabel("GObject", box);
-		lineXi = box.getX() + BOX_WIDTH / 2;
-		lineYi = box.getY() + BOX_HEIGHT;
+		Xi = box.getX() + BOX_WIDTH / 2;
+		Yi = box.getY() + BOX_HEIGHT;
 	}
 	
+	/* Method: drawLabel() */
+	/**
+	 * Draws and adds a GLabel.
+	 * @param title the title of the label
+	 * @param box the box where the label will be positioned
+	 */
 	private void drawLabel(String title, GRect box) {
 		GLabel label = new GLabel(title);
 		label.setFont("Monaco-bold-24");
@@ -69,10 +108,20 @@ public class GraphicsHierarchy extends GraphicsProgram {
 		add(label);
 	}
 
+	/* Method: drawBox() */
+	/**
+	 * Draws and returns a GRect.
+	 * @return a GRect
+	 */
 	private GRect drawBox() {
 		GRect box = new GRect(0, 0, BOX_WIDTH, BOX_HEIGHT);
 		add(box);
 		return box;
 	}
+	
+	private final int BOX_WIDTH = 150;
+	private final int BOX_HEIGHT = 50;
+	
+	double Xi, Yi;
 
 }
